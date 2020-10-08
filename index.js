9d22b1f26e59f9823d9ecc3fd9e2962a708abc12
Marcin Kowalicki <kowalickimarcin13@gmail.com>
2020-10-08 21:41:51 +0200

    Add searcher

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-var req = new XMLHttpRequest();
-req.open('GET', 'https://raw.githubusercontent.com/web-radio/webradio/master/radios.json', false); 
-req.send(null);
-const radios = JSON.parse(req.response)
+// req.open('GET', 'https://raw.githubusercontent.com/web-radio/webradio/master/radios.json', false); 
+//req.send(null);
+// const radios = JSON.parse(req.response)
 
+let radios
+const searchButton = document.getElementById("searchbutton");
 const content = document.querySelector('#content');
 
-radios.forEach((elem) => {
-		if(elem.last) {
-			content.innerHTML += `<p>${elem.name}</p><audio src="${elem.src}" type="${elem.type}" preload="none" class="radioElementLast" controls></audio>`;
-		} else {
-			content.innerHTML += `<p>${elem.name}</p><audio src="${elem.src}" type="${elem.type}" preload="none" class="radioElement" controls></audio>`;
-		}
+searchButton.addEventListener("click", () => {
+	content.innerHTML = ''
+	var req = new XMLHttpRequest();
+	const searcher = document.getElementById("search").value
+  req.open('GET', `https://de1.api.radio-browser.info/json/stations/byname/${searcher}`, false); 
+  req.send(null);
+  let radios = JSON.parse(req.response)
+	
+	console.log(radios)
+
+	radios.forEach((elem) => {
+		content.innerHTML += `<p>${elem.name}</p><audio src="${elem.url_resolved}" type="${elem.codec}" preload="none" class="radioElementLast" controls></audio>`
 });
+});
+
