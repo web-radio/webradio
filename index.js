1074276cd0f3eb1f31fb4cbf637fe99736de8160
Marcin Kowalicki <kowalickimarcin13@gmail.com>
2020-10-08 22:50:38 +0200

    Restore importing from radios.json

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-// req.open('GET', 'https://raw.githubusercontent.com/web-radio/webradio/master/radios.json', false); 
-//req.send(null);
-// const radios = JSON.parse(req.response)
-
 let radios
 const searchButton = document.getElementById("searchbutton");
 const content = document.querySelector('#content');
+const searcher = document.getElementById("search").value
+var req = new XMLHttpRequest();
 
 searchButton.addEventListener("click", () => {
 	content.innerHTML = ''
-	var req = new XMLHttpRequest();
-	const searcher = document.getElementById("search").value
+	if(searcher == "radios.json") {
+		req.open('GET', 'https://raw.githubusercontent.com/web-radio/webradio/master/radios.json', false); 
+		req.send(null);
+		const list = JSON.parse(req.response)
+		list.forEach((elem) => {
+		content.innerHTML += `<p>${elem.name}</p><audio src="${elem.url_resolved}" type="${elem.codec}" preload="none" class="radioElementLast" controls></audio>`
+});
+	}
+	
   req.open('GET', `https://de1.api.radio-browser.info/json/stations/byname/${searcher}`, false); 
   req.send(null);
   let radios = JSON.parse(req.response)
