ad135bc05d7e2a3964d94eae42b6372555de1d7f
Marcin Kowalicki <kowalickimarcin13@gmail.com>
2021-02-01 23:26:45 +0100

    lol. this project still working?!

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-let radios
 const searchButton = document.getElementById("searchbutton");
 const content = document.querySelector('#content');
 const searcher = document.getElementById("search");
-const req = new XMLHttpRequest();
 
 searchButton.addEventListener("click", () => {
     content.innerHTML = ''
-    if (searcher.value === "radios.json") {
-        req.open('GET', 'https://raw.githubusercontent.com/web-radio/webradio/master/radios.json', false);
-        req.send(null);
-        const list = JSON.parse(req.response)
-        list.forEach((elem) => {
-            content.innerHTML += `<p>${elem.name}</p><audio src="${elem.url_resolved}" type="${elem.codec}" preload="none" class="radioElementLast" controls></audio>`
-        });
+    if (searcher.value === "radios.json" || searcher.value === "all" || searcher.value === "wszystkie") {
+        fetch("https://raw.githubusercontent.com/web-radio/webradio/master/radios.json")
+            .then(response => response.json())
+            .then(data => {
+                data.forEach((elem) => {
+                    content.innerHTML += `<p>${elem.name}</p><audio src="${elem.src}" type="${elem.codec}" preload="none" class="radioElementLast" controls></audio>`
+                })
+            })
     } else {
-        console.log(searcher.value)
-        req.open('GET', `https://de1.api.radio-browser.info/json/stations/byname/${searcher.value}`, false);
-        req.send(null);
-        let radios = JSON.parse(req.response)
-
-        console.log(radios)
-
-        if (radios.length === "0") {
-            content.style.display = "block";
-            content.innerHTML += `<p style="margin-left:auto;margin-right:auto;">Nie znaleziono żadnych stacji!</p>`
-        }
-
-        radios.forEach((elem) => {
-            content.style.display = "grid";
-            content.innerHTML += `<p>${elem.name}</p><audio src="${elem.url_resolved}" type="${elem.codec}" preload="none" class="radioElementLast" controls></audio>`
-        });
+        fetch(`https://de1.api.radio-browser.info/json/stations/byname/${searcher.value}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.length === 0) {
+                    content.style.display = "block";
+                    content.innerHTML += `<p style="margin-left:auto;margin-right:auto;">Nie znaleziono żadnych stacji!</p>`
+                } else {
+                    data.forEach((elem) => {
+                        content.style.display = "grid";
+                        content.innerHTML += `<p>${elem.name}</p><audio src="${elem.url_resolved}" type="${elem.codec}" preload="none" class="radioElementLast" controls></audio>`
+                    });
+                }
+            })
     }
 });
 
