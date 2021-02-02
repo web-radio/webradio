f1ca1aa0f807576c6a99c525867057c70ef2c471
Marcin Kowalicki <kowalickimarcin13@gmail.com>
2021-02-02 10:15:14 +0100

    notatka mała

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const searchButton = document.getElementById("searchbutton");
 const content = document.querySelector('#content');
+const note = document.getElementById("note")
 const searcher = document.getElementById("search");
 
 searchButton.addEventListener("click", () => {
@@ -11,7 +12,11 @@ searchButton.addEventListener("click", () => {
                 data.forEach((elem) => {
                     content.innerHTML += `<div><p>${elem.name}</p><audio src="${elem.src}" type="${elem.codec}" preload="none" class="radioElementLast" controls></audio></div>`
                 })
+                note.innerHTML = ""
             })
+    } else if(searcher.value == "") {
+        content.style.display = "block";
+        content.innerHTML += `<p style="margin-left:auto;margin-right:auto;">Myśl trochę. Nic nie wpisałeś.</p>`
     } else {
         fetch(`https://de1.api.radio-browser.info/json/stations/byname/${searcher.value}`)
             .then(response => response.json())
@@ -22,8 +27,9 @@ searchButton.addEventListener("click", () => {
                 } else {
                     data.forEach((elem) => {
                         content.style.display = "grid";
-                        content.innerHTML += `<div><p>${elem.name}</p><audio src="${elem.url_resolved}" type="${elem.codec}" preload="none" controls></audio></div>`
+                        content.innerHTML += `<div data-uuid="${elem.stationuuid}"><p>${elem.name}</p><audio src="${elem.url_resolved}" type="${elem.codec}" preload="none" controls></audio><input type="checkbox"></div>`
                     });
+                    note.innerHTML = "<p>Uwaga: jeśli pojawia się kilka stacji z tą samą nazwą, zazwyczaj mają one rózny <a style=\"display:inline\" href=\"https://pl.wikipedia.org/wiki/Przep%C5%82ywno%C5%9B%C4%87\">bitrate</a>.</p>"
                 }
             })
     }
