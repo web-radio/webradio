10a3365727866bd522fb7b0ba4171560a62a5268
Marcin Kowalicki <kowalickimarcin13@gmail.com>
2021-02-03 15:05:41 +0100

    musze dodac przelacznik do polskiego bo angielski juz jest

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+let lang
 const searchButton = document.getElementById("searchbutton");
 const content = document.querySelector('#content');
 const note = document.getElementById("note")
 const searcher = document.getElementById("search");
 
 const subheader = document.querySelector("#subheader")
+const searchLabel = document.querySelector("#searchLabel")
+const legacyInfo = document.querySelector("#legacyInfo")
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("https://raw.githubusercontent.com/web-radio/webradio/master/languages/english.json")
+    fetch("https://raw.githubusercontent.com/web-radio/webradio/master/languages/polish.json")
         .then(response => response.json())
         .then(data => {
+            lang = data
             subheader.textContent = data.header
+            searchLabel.textContent = data.searchText
+            legacyInfo.textContent = data.legacyInfo
             console.log(data)
         })
 })
@@ -27,20 +33,21 @@ searchButton.addEventListener("click", () => {
             })
     } else if(searcher.value == "") {
         content.style.display = "block";
-        content.innerHTML += `<p style="margin-left:auto;margin-right:auto;">Think a little. You didn't enter anything.</p>`
+        content.innerHTML += `<p style="margin-left:auto;margin-right:auto;">${lang.nothingHere}</p>`
     } else {
         fetch(`https://de1.api.radio-browser.info/json/stations/byname/${searcher.value}`)
             .then(response => response.json())
             .then(data => {
                 if (data.length === 0) {
                     content.style.display = "block";
-                    content.innerHTML += `<p style="margin-left:auto;margin-right:auto;">No stations found!</p>`
+                    content.innerHTML += `<p style="margin-left:auto;margin-right:auto;">${lang.noStations}</p>`
+                    note.innerHTML = ""
                 } else {
                     data.forEach((elem) => {
                         content.style.display = "grid";
                         content.innerHTML += `<div data-uuid="${elem.stationuuid}"><p>${elem.name}</p><p>Bitrate: ${elem.bitrate}kbps</p><audio src="${elem.url_resolved}" type="${elem.codec}" preload="none" controls></audio>`
                     });
-                    note.innerHTML = "<p>Note: if several stations with the same name appear, they usually have a different <a style=\"display:inline\" href=\"https://en.wikipedia.org/wiki/Bit_rate\">bitrate</a>.</p>"
+                    note.innerHTML = `<p>${lang.bitrateInfo} <a style=\"display:inline\" href=\"https://en.wikipedia.org/wiki/Bit_rate\">bitrate</a>.</p>`
                 }
             })
     }
